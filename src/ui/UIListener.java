96b0a58c93e7712e818387fd69e6527591da2a1b
borislav-milkov <milkovborislav@gmail.com>
2017-06-20 23:28:06 -0700

    Fixed save and load

---
+++ src/ui/UIListener.java
@@ -0,0 +1,15 @@
+package ui;
+//package ui;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+public interface UIListener
+{
+	public void setImage(BufferedImage iamge, String name);
+	public void segment(Point source, Point[] sinks);
+	public void loadGraph(File f);
+	public void saveGraph(File f);
+	public void saveSegment();
+	public String displayGraph();
+}
